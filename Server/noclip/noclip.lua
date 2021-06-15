26f4e78a0ad85aa2a05f65f328df1d5ff6435014
Xalalau Xubilozo <xalalau@outlook.com>
2021-06-15 20:43:09 -0300

    Improve noclip landing code

--- Server/noclip/noclip.lua
+++ Server/noclip/noclip.lua
@@ -1,3 +1,27 @@
+-- Don't process falling damage when the player first hits the ground after exiting noclip
+local function SetLanding(possession)
+    if possession:GetValue("LL_Noclip_Land") then return end
+
+    possession:SetValue("LL_Noclip_Land", true)
+    possession:SetFallDamageTaken(0)
+
+    Character:Subscribe("FallingModeChanged", function(possession, _, new_state)
+        if new_state == 0 then
+            local my_particle = Particle(
+                possession:GetLocation() - Vector(0, 0, 100),
+                Rotator(0, 0, 0),
+                "NanosWorld::P_Explosion",
+                true,
+                true
+            )
+    
+            possession:SetValue("LL_Noclip_Land", false)
+            possession:SetFallDamageTaken(10)
+            possession:Unsubscribe("FallingModeChanged")
+        end
+    end)
+end
+
 -- Set Noclip
 Events:Subscribe("LL_SetNoclip", function(player)
     local possession = player:GetControlledCharacter()
@@ -10,24 +34,6 @@ Events:Subscribe("LL_SetNoclip", function(player)
     possession:SetFlyingMode(state)
     possession:SetValue("LL_flyingMode", state, true)
 
-    -- Prepare 
-    if not state and not possession:GetValue("LL_falling") then
-        possession:SetValue("LL_falling", true)
-        possession:SetFallDamageTaken(0)
-    end
-end)
-
-Character:Subscribe("FallingModeChanged", function(possession, _, new_state)
-    if new_state == 0 and possession:GetValue("LL_falling") then
-        local my_particle = Particle(
-            possession:GetLocation() - Vector(0, 0, 100),
-            Rotator(0, 0, 0),
-            "NanosWorld::P_Explosion",
-            true,
-            true
-        )
-
-        possession:SetValue("LL_falling", false)
-        possession:SetFallDamageTaken(10)
-    end
+    -- Set landing
+    SetLanding(possession)
 end)
